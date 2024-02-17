0a4470343bbfc007f42f6cc5f197e364ec398465
mdymi <dyminski.michal@outlook.com>
2024-02-17 21:32:51 +0100

    Następna lekcja

---
+++ Brzezinski/conditional instructions.py
@@ -0,0 +1,19 @@
+# #example 1:
+#
+# age = int(input("Ile masz lat?"))
+#
+# if age >= 18:
+#     print("Jesteś pełnoletni")
+# else:
+#     print("Jesteś niepełnoletni")
+
+#example 2:
+
+light = input("Jakie jest światło ? (zielone,pomarańczowe,czerwone)")
+if light == 'zielone':
+     print("Możesz jechać")
+elif light == 'pomarańczowe':
+    print("Zwolnij!")
+elif light == 'czerwone':
+    print("Stój")
+
